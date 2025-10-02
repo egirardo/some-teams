0a065ff8ba10d19a6c9c600b9280a2dc4a79f0b9
Elsa Girardo <egirardo@hotmail.com>
2025-10-02 10:10:55 +0200

    Added header, footer, index, and about files to repo

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Professional Women's Soccer Teams";
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
+    <header>
+        <h1><?php echo $title ?>
+            <nav>
+                <a href="<?php echo "index.php" ?>">Home</a>
+                <a href="<?php echo "about.php" ?>">About</a>
+            </nav>
+    </header>
