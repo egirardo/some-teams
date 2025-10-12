e55ce597d565eeb92efbe70958c46519ad9fcab0
Elsa Girardo <egirardo@hotmail.com>
2025-10-12 17:11:31 +0200

    Feature: Added logic for dynamic pages based upon teams

--- teams.php
+++ teams.php
@@ -4,41 +4,38 @@ require "header.php";
 require "data.php";
 
 $id = $_GET["id"];
-// $team = $teams[$id];
+
+if (isset($_GET["id"])) {
+    $team = $_GET["id"];
+    $id = $teams[$team];
+}
 
 ?>
 
 <div class="team-info">
-    <?php foreach ($teams as $team => $data) {
-        // foreach ($team as $key => $value) { 
-    ?>
-        <h1 class="name"><?php echo $id; ?></h1>
-        <div class="logo-team-pg">
-
-        </div>
-        <div class="league-info">
-            <h2 class="league-title">League:</h2>
-            <p><?php echo $data['league']; ?></p>
-        </div>
-        <div class="uefa-info">
-            <h2>UEFA Coefficient Ranking:</h2>
-            <p><?php echo $data['uefa-coefficient-ranking']; ?></p>
-        </div>
-        <div class="city-info">
-            <h2>City:</h2>
-            <p><?php echo $data['city']; ?></p>
-        </div>
-        <div class="opponents-info">
-            <h2>Opponents:</h2>
-            <ul>
-                <?php foreach ($data['opponents'] as $opponent) { ?>
-                    <li><?php echo $opponent ?></li>
-                <?php } ?>
-            </ul>
-        </div>
-        <!-- add back php } if secondary for each loop is needed above -->
-    <?php } ?>
+    <h1 class="name"><?php echo $team; ?></h1>
+    <div class="logo-team-pg">
+        <img class="logo" src="<?= $id['logo']; ?>" alt="Logo">
+    </div>
+    <div class="league-info">
+        <h2 class="league-title">League:</h2>
+        <p><?php echo $id['league']; ?></p>
+    </div>
+    <div class="uefa-info">
+        <h2>UEFA Coefficient Ranking:</h2>
+        <p><?php echo $id['uefa-coefficient-ranking']; ?></p>
+    </div>
+    <div class="city-info">
+        <h2>City:</h2>
+        <p><?php echo $id['city']; ?></p>
+    </div>
+    <div class="opponents-info">
+        <h2>Opponents:</h2>
+        <ul>
+            <?php foreach ($id['opponents'] as $opponent) { ?>
+                <li><?php echo $opponent ?></li>
+            <?php } ?>
+        </ul>
+    </div>
 </div>
-
-<!-- if footer is not showing it's because the team info isn't loading correctly -->
 <?php require "footer.php"; ?>
