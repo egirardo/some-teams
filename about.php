4e66df7549def1d763d8fe1b6b81d434c01331db
Elsa Girardo <egirardo@hotmail.com>
2025-10-13 18:42:47 +0200

    Feature: Added content and image to about.php

--- about.php
+++ about.php
@@ -2,9 +2,18 @@
 define("TITLE", "About");
 require "header.php";
 ?>
-<div class="about">
-    <h1>About Me</h1>
-
+<div class="about-container">
+    <div class="about-heading">
+        <h1 class="about-title">About The Creator</h1>
+    </div>
+    <div class="content-photo-container">
+        <div class="about-content">
+            <p class="description">My name is Elsa Girardo and I am a web developer currently studying web development at Yrgo in Gothenburg, Sweden. I have worked alongside a team of developers as their business analyst for the last two years and have now decided to pivot to learning to code. I have created this website as an assignment for our class called "Some Teams". The assignment was to take the information provided to us in a multidimensional array that can be found <a href="https://github.com/egirardo/some-teams/blob/main/data.php">here</a> and create a website that displays all of the given data. I hope you enjoy the way that information has been laid out for you!</p>
+        </div>
+        <div class=" headshot-container">
+            <img class="headshot" src="elsa.png" alt="black and white image of the author">
+        </div>
+    </div>
 </div>
 
 <?php require "footer.php"; ?>
